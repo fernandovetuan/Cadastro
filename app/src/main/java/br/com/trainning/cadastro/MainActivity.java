0e603d94ca4445bd73adf0a9617460bc560b18b4
fernandovetuan <fernando.vertuan@gmail.com>
2016-02-27 16:42:57 -0300

    alterado nome do app

--- app/src/main/java/br/com/trainning/cadastro/MainActivity.java
+++ app/src/main/java/br/com/trainning/cadastro/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.trainning.cadastro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,22 +9,39 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editTextNome;
+    EditText editTextIdade;
+    EditText editTextTelefone;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        editTextNome = (EditText) findViewById(R.id.txtNome);
+        editTextIdade = (EditText) findViewById(R.id.txtIdade);
+        editTextTelefone = (EditText) findViewById(R.id.txtTelefone);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String nome = editTextNome.getText().toString();
+                String idade = editTextIdade.getText().toString();
+                String telefone = editTextTelefone.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this,Tela2Activity.class);
+
             }
         });
     }
